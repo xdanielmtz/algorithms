f31fe07bb11b4a085569c729ae9c9e8ed401d249
Dani JM <xdanielmtz@gmail.com>
2021-02-26 20:43:30 -0500

    wrote code to solve multiplication

--- arethmetic/arithmetic.js
+++ arethmetic/arithmetic.js
@@ -8,6 +8,8 @@ var subtract = function(num1, num2) {
     return num1 - num2;
 };
 
-var multiply = function(num1, num2) {};
+var multiply = function(num1, num2) {
+    return num1 * num2;
+};
 
 var divide = function(num1, num2) {};
